84b0b3cb13f11e3106f8382c2b4541c1dc1367b0
nkumar <nkumar@itradenetwork.com>
2018-05-09 13:43:17 +0530

    heroku changes

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const express = require("express");
 
 /* we will do our customizations for our app variable below.*/
 const app = express();
-
+const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, "/public")));
 
-app.listen(3000, () => {
-    console.log("Server listening on port 3000!!");
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}!!`);
 });
 
